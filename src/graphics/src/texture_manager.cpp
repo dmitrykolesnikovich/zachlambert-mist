bb669b768bfbef0ae46ce6bfea3c6cd9a2fce9fd
Zach Lambert <zachalambert@gmail.com>
2020-07-23 12:21:08 +0100

    Runs, but doesn't show anything

--- src/graphics/src/texture_manager.cpp
+++ src/graphics/src/texture_manager.cpp
@@ -1,5 +1,7 @@
 #include "texture_manager.h"
 
+#include <iostream>
+
 #include <GL/glew.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -10,7 +12,6 @@ unsigned int load_texture(const std::string &texture_path)
     int width, height, num_channels;
     unsigned char *data = stbi_load(texture_path.c_str(), &width, &height, &num_channels, 0);
     if (!data) {
-        printf("Failed to load image %s.\n", texture_path.c_str());
         return 0;
     } else {
         GLuint texture_id;
@@ -22,16 +23,21 @@ unsigned int load_texture(const std::string &texture_path)
                     GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
                     width, height, 0, GL_DEPTH_COMPONENT,
                     GL_UNSIGNED_BYTE, data);
+                break;
             case 3: // rgb
                 glTexImage2D(
                     GL_TEXTURE_2D, 0, GL_RGB,
                     width, height, 0, GL_RGB,
                     GL_UNSIGNED_BYTE, data);
+                break;
             case 4: // rgba
                 glTexImage2D(
                     GL_TEXTURE_2D, 0, GL_RGBA,
                     width, height, 0, GL_RGBA,
                     GL_UNSIGNED_BYTE, data);
+                break;
+            default:
+                return 0;
         }
         glGenerateMipmap(GL_TEXTURE_2D);
         // Always repeat the image
