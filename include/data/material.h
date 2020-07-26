f37c1a1c9810c2264d1151d47b61d553e74ddc7d
Zach Lambert <zachalambert@gmail.com>
2020-07-26 19:22:38 +0100

    Changed material to store texture path instead

--- include/data/material.h
+++ include/data/material.h
@@ -1,8 +1,8 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include <string>
 #include <glm/glm.hpp>
-
 #include "data/texture.h"
 
 namespace mist {
@@ -15,16 +15,10 @@ enum class MaterialType {
 // Use pointers to textures, to allow for a null pointer
 // when not using textures
 struct Material {
-    union {
-    struct { //COLORED
-        glm::vec3 diffuse_color;
-        glm::vec3 specular_color;
-    };
-    struct { // TEXTURED
-        const Texture *diffuse_texture;
-        const Texture *specular_texture;
-    };
-    };
+    glm::vec3 diffuse_color;
+    std::string diffuse_texture;
+    glm::vec3 specular_color;
+    std::string specular_texture;
     MaterialType type;
 };
 
